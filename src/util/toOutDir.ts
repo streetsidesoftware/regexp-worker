1e231b22c4817cb7e3bb72f5b2d44d6af039cff3
Jason Dent <jason@streetsidesoftware.nl>
2020-05-24 15:24:49 +0200

    Add test to toOutDir

--- src/util/toOutDir.ts
+++ src/util/toOutDir.ts
@@ -3,7 +3,7 @@ import * as Path from 'path';
 const outDir = 'lib';
 
 export function toOutDir(filename: string): string {
-    if (!filename || /\bsrc$/.test(filename)) return filename.replace(/\bsrc$/, outDir);
+    if (!filename || !Path.basename(filename) || /\bsrc$/.test(filename)) return filename.replace(/\bsrc$/, outDir);
     return Path.join(
         toOutDir(Path.dirname(filename)),
         Path.basename(filename)
