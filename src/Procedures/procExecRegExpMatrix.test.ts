c58089235c5dca871ed52e7299fb19ea8e1cc7e1
Jason Dent <jason@streetsidesoftware.nl>
2020-05-27 08:48:15 +0200

    Add support for evaulating a Regular Expression Matrix

---
+++ src/Procedures/procExecRegExpMatrix.test.ts
@@ -0,0 +1,22 @@
+import { createRequestExecRegExpMatrix, procExecRegExpMatrix, isExecRegExpMatrixResponse, } from './procExecRegExpMatrix';
+import { Request } from './procedure';
+import { createId } from './uniqueId';
+
+describe('procExecRegExpMatrix', () => {
+    test('basic', () => {
+        const req = createRequestExecRegExpMatrix({ textArray: ['two words'], regExpArray: [/w\w+/g] });
+        const result = procExecRegExpMatrix(req);
+        expect(isExecRegExpMatrixResponse(result)).toBe(true);
+        const response = isExecRegExpMatrixResponse(result) ? result : undefined;
+        expect(response?.data.elapsedTimeMs).toBeGreaterThan(0);
+        expect(response?.data.matrix).toHaveLength(1);
+        expect(response?.data.matrix[0].results[0].matches.map(m => m[0])).toEqual(['wo', 'words'])
+    });
+
+    test('non-RequestExecRegExpMatrix', () => {
+        const req: Request = { id: createId(), requestType: 'unknown', data: { text: 'two words', regexp: /w\w+/g }};
+        const result = procExecRegExpMatrix(req);
+        expect(isExecRegExpMatrixResponse(result)).toBe(false);
+        expect(result).toBeUndefined();
+    });
+});
--- src/Procedures/procedures.ts
+++ src/Procedures/procedures.ts
@@ -3,11 +3,13 @@ import { procEcho } from './procEcho';
 import { procExecRegExp } from './procExecRegExp';
 import { procSleep } from './procSleep';
 import { procSpin } from './procSpin';
+import { procExecRegExpMatrix } from './procExecRegExpMatrix';
 
 type Procedure = (r: Request) => Promise<Response> | Response | undefined;
 
 export const procedures: Procedure[]  = [
     procExecRegExp,
+    procExecRegExpMatrix,
     procEcho,
     procSleep,
     procSpin,
