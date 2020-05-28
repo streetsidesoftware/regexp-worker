6213d280a9eb706ce8e8b336e40673be76112f3c
Jason Dent <jason@streetsidesoftware.nl>
2020-05-28 23:30:48 +0200

    Update index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,8 @@
-import {} from '.';
-import { RegExpWorker } from './RegExpWorker';
+import { RegExpWorker } from '.';
 
 describe('Validate Index', () => {
     test('quick test RegExpWorker', run(async (w) => {
-        const r = await w.execRegExp(/./g, 'hello');
+        const r = await w.execRegExp(/./g, 'hello', 500);
         expect(r.elapsedTimeMs).toBeGreaterThan(0);
         expect(r.elapsedTimeMs).toBeLessThan(10);
         expect(r.matches.map(m => m[0])).toEqual('hello'.split(''));
