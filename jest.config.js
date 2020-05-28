e0d0e2943d2883fa77e8b2a3cb03ac9ff9b9c466
Jason Dent <jason@streetsidesoftware.nl>
2020-05-28 23:00:35 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const os = require('os');
 const useTSJest = !process.env['TEST_JS'];
 let custom = {};
 if (useTSJest) {
@@ -12,6 +14,15 @@ if (useTSJest) {
     console.log('test-js');
     custom = { roots: ['./lib'], }
 }
+
+if ((/windows/i).test(os.type())) {
+    console.log(os.type())
+    console.log('Max Concurrency set to 1')
+    custom.maxConcurrency = 1;
+} else {
+    console.log(os.type())
+}
+custom.maxConcurrency = 1;
 
 module.exports = {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec|perf))\\.[tj]sx?$',
