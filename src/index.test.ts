79ef3eb67b1f04468a76798c7307144661a431c4
Jason Dent <jason@streetsidesoftware.nl>
2020-05-28 23:24:15 +0200

    Update index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,12 +2,14 @@ import {} from '.';
 import { RegExpWorker } from './RegExpWorker';
 
 describe('Validate Index', () => {
-    test('quick test RegExpWorker', async () => {
-        const w = new RegExpWorker();
+    test('quick test RegExpWorker', run(async (w) => {
         const r = await w.execRegExp(/./g, 'hello');
         expect(r.elapsedTimeMs).toBeGreaterThan(0);
         expect(r.elapsedTimeMs).toBeLessThan(10);
         expect(r.matches.map(m => m[0])).toEqual('hello'.split(''));
-        await w.dispose();
-    });
+    }));
 });
+
+function run(fn: (w: RegExpWorker) => Promise<any>, w = new RegExpWorker()): () => Promise<void> {
+    return () => fn(w).finally(w.dispose).then();
+}
