8db4334f9139eb2bbf4c3d3f37f3ad32b1d1f219
Jason Dent <Jason3S@users.noreply.github.com>
2020-12-22 19:27:49 +0100

    ci: update packages (#17)

--- src/worker/MessagePort.ts
+++ src/worker/MessagePort.ts
@@ -1,14 +1,17 @@
 export interface MessagePort {
-    postMessage(value: any, transferList?: Array<ArrayBuffer | MessagePort>): void;
+    postMessage(value: any): void;
     on(event: 'close', listener: () => void): this;
     on(event: 'message', listener: (value: any) => void): this;
     off(event: 'close', listener: () => void): this;
     off(event: 'message', listener: (value: any) => void): this;
 }
 
-
 export const nullPort: MessagePort = Object.freeze({
     postMessage() {},
-    on() { return nullPort },
-    off() { return nullPort },
-})
+    on() {
+        return nullPort;
+    },
+    off() {
+        return nullPort;
+    },
+});
