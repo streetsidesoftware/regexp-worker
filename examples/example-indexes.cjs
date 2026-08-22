2c616a5a9e85c282c746b590d1ea6986f264e7fb
Jason Dent <jason@streetsidesoftware.nl>
2026-08-22 08:53:52 +0200

    Remove support for commonjs

--- examples/example-indexes.cjs
+++
@@ -1,8 +0,0 @@
-const { workerMatchAll } = require('regexp-worker');
-
-async function run() {
-    const response = await workerMatchAll('Good Morning', /\b/g);
-    console.log(response.matches.map((m) => m.index));
-}
-
-run();
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig([
     { entry: ['./src/index.mts'], platform: 'node', outDir: './dist/', format: 'esm' },
-    { entry: ['./src/index.mts'], platform: 'node', outDir: './dist/', format: 'cjs' },
     { entry: ['./src/browser.ts'], platform: 'browser', outDir: './dist/' },
 ]);
